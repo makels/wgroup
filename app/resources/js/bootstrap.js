941f685555851e57f01a62209c53992150df551f
Sergey Shulha <sv_shulga@ukr.net>
2022-08-16 14:40:14 +0300

    last changes

--- app/resources/js/bootstrap.js
+++ app/resources/js/bootstrap.js
@@ -3,7 +3,7 @@ window._ = require('lodash');
 try {
     require('bootstrap');
 } catch (e) {}
-
+window.Vue = require("vue/dist/vue.min");
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
