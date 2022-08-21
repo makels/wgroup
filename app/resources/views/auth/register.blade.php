5ebbc3e76bdbec2a7d442b58099f882e8509d55b
Sergey Shulha <sv_shulga@ukr.net>
2022-08-21 08:37:05 +0300

    last changes

--- app/resources/views/auth/register.blade.php
+++ app/resources/views/auth/register.blade.php
@@ -26,6 +26,19 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Birthday') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="birthday" type="text" class="form-control @error('birthday') is-invalid @enderror" name="birthday" value="{{ old('birthday') }}" required autocomplete="birthday">
+                                @error('birthday')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
@@ -74,4 +87,5 @@
         </div>
     </div>
 </div>
+
 @endsection
